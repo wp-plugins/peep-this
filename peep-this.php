573579c6289ece2ac4c0997831a2cc919501d0c0
Seags <Seags@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-03 23:44:46 +0000

    Version 1 of Peep This
    git-svn-id: https://plugins.svn.wordpress.org/peep-this/trunk@115379 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ peep-this.php
@@ -0,0 +1,320 @@
+<?php
+/*
+Plugin Name: Peep This
+Plugin URI: http://tx-daily.co.za/peep-this
+Description: Adds a "Peep This" button to every post and page. Shortens URLs in advance. Customize under Settings > Peep This.
+Author: Tx Daily (Seagyn Davis)
+Version: 0.1
+Author URI: http://www.txdaily.co.za/
+Text Domain: peep-this
+*/
+
+/*
+Peep This is a plugin for WordPress 1.5 - 2.7.1 and WordPress MU.
+Copyright 2009  Tx Daily  (email : sdavis@txsolutions.co.za)
+Freely released under Version 2 of the GNU General Public License as published
+by the Free Software Foundation, or, at your option, any later version.
+*/
+
+
+function pt_option($key) {
+	$options = get_option('peep_this_settings'); return $options[$key];
+}
+
+function pt_read_file($url) {
+	if (ini_get('allow_url_fopen') == 1 || ini_get('allow_url_fopen') ==
+	'on' ||	ini_get('allow_url_fopen') == 'On') {
+		$file = @file_get_contents($url); if ($file == false) {
+			$handle = fopen($url, 'r');
+			$file = fread($handle, 4096); fclose($handle);}} else {
+		if (function_exists('curl_init')) {$ch = curl_init($url);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,5);
+		$file = curl_exec($ch);	curl_close($ch);}}
+	if ($file != false && $file != '') return $file;
+}
+
+
+function peep_this_short_url() {
+	global $id; $purl = get_permalink();
+	$cached_url = get_post_meta($id, 'peep_this_url', true);
+	if($cached_url && $cached_url != 'getnew') return $cached_url;
+	else {
+		$url = pt_read_file('http://gatorurl.com/api/rest.php?url=' . $purl);		
+		if ($cached_url == 'getnew')
+		update_post_meta($id, 'peep_this_url', $url, 'getnew');
+		else add_post_meta($id, 'peep_this_url', $url, true);
+	}
+	return $url;
+}
+
+function peep_this_trim_title() {
+	$title = get_the_title();
+	$special = array('&#34;', '&#034;', '&#38;', '&#038;', '&#39;',
+	'&#039;', '&#60;', '&#060;', '&#62;', '&#062;', '&#160;', '&#161;',
+	'&#162;', '&#163;', '&#164;', '&#165;', '&#166;', '&#167;', '&#168;',
+	'&#169;', '&#170;', '&#171;', '&#172;', '&#173;', '&#174;', '&#175;',
+	'&#176;', '&#177;', '&#178;', '&#179;', '&#180;', '&#181;', '&#182;',
+	'&#183;', '&#184;', '&#185;', '&#186;', '&#187;', '&#188;', '&#189;',
+	'&#190;', '&#191;', '&#192;', '&#193;', '&#194;', '&#195;', '&#196;',
+	'&#197;', '&#198;', '&#199;', '&#200;', '&#201;', '&#202;', '&#203;',
+	'&#204;', '&#205;', '&#206;', '&#207;', '&#208;', '&#209;', '&#210;',
+	'&#211;', '&#212;', '&#213;', '&#214;', '&#215;', '&#216;', '&#217;',
+	'&#218;', '&#219;', '&#220;', '&#221;', '&#222;', '&#223;', '&#224;',
+	'&#225;', '&#226;', '&#227;', '&#228;', '&#229;', '&#230;', '&#231;',
+	'&#232;', '&#233;', '&#234;', '&#235;', '&#236;', '&#237;', '&#238;',
+	'&#239;', '&#240;', '&#241;', '&#242;', '&#243;', '&#244;', '&#245;',
+	'&#246;', '&#247;', '&#248;', '&#249;', '&#250;', '&#251;', '&#252;',
+	'&#253;', '&#254;', '&#255;', '&#8211;', '&#8212;', '&#8216',
+	'&#8217;', '&#8220;', '&#8221;', '&#8230;', '&#8482;', '&#8243;',
+	'&amp;', '&gt;', '&lt;', '&quot;', '’', '“', '”');
+	$normal = array('"', '"', '&', '&', '\'', '\'', '<', '<', '>', '>',
+	' ', '¡', '¢', '£', '¤', '¥', '¦', '§', '¨', '©', 'ª', '«', '¬', '',
+	'®', '¯', '°', '±', '²', '³', '´', 'µ', '¶', '·', '¸', '¹', 'º', '»',
+	'¼', '½', '¾', '¿', 'À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É',
+	'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', '×',
+	'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'Þ', 'ß', 'à', 'á', 'â', 'ã', 'ä', 'å',
+	'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ñ', 'ò', 'ó',
+	'ô', 'õ', 'ö', '÷', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'þ', 'ÿ', '--', '--',
+	'\'', '\'', '"', '\'\'', '...', '\'', '"', '&', '>', '<', '"', '\'',
+	'"', '"');
+	$title = str_replace($special, $normal, $title);
+	$title = $title . ' ';
+	$url_len = '26';
+	$len1 = (135 - $url_len); $len2 = ($len1 - 5);
+	$len3 = ($len1 + 5); $title = substr($title,0,$len1);
+	$title = substr($title,0,strrpos($title,' '));
+	if (strlen($title) > $len2 && strlen($title) < $len3)
+	$title = $title . ' ...'; $title = urlencode($title);
+	return $title;
+}
+
+// is_preview doesn't exist in WP 1.5.
+function pt_is_preview() {if (function_exists('is_preview')) is_preview();}
+
+function pt_display_limits($item) {
+	if (!pt_is_preview()) {
+		if (pt_option('pt_limit_to_posts') == 'true') {
+			if (pt_option('pt_limit_to_single') == 'true') {
+				if (is_single()) return $item;}
+			else {if (!is_page()) return $item;}}
+		if (pt_option('pt_limit_to_single') == 'true') {
+			if (pt_option('pt_limit_to_posts') == 'true') {
+				if (is_single()) return $item;}
+			else {if (is_single() || is_page()) return $item;}}
+		if (pt_option('pt_limit_to_posts') != 'true' &&
+		pt_option('pt_limit_to_single') != 'true') return $item;
+	}
+}
+
+function peep_this_url($peep_text = '') {
+	$peep_title = peep_this_trim_title();
+	$peep_url = peep_this_short_url();
+	$item = 'http://gatorpeeps.com/?url='.$peep_url.'&txt='.$peep_title;
+	return pt_display_limits($item);
+}
+
+function peep_this() {
+	if ($img_class == '') $img_class = 'nothumb';
+	$url = peep_this_url($peep_text);
+	if ($icon_file == '') {
+		if (pt_option('pt_gatorpeeps_icon') == '') $icon_file = 'pt-gatorpeeps.png';
+		else $icon_file = pt_option('pt_gatorpeeps_icon');
+	}
+	$icon = get_option('siteurl').'/wp-content/plugins/peep-this/icons/'.$icon_file;
+	$item = '<a class="peep-this" href="'.$url.'" title="Peep on Gatorpeeps"><img class="nothumb" src="'.
+			$icon.'" alt="Peep on Gatorpeeps" border="0" /></a>';
+	return pt_display_limits($item);
+}
+
+function insert_peep_this($content) {
+	global $id;
+	$peep_this_hide = get_post_meta($id, 'peep_this_hide', true);
+	if ($peep_this_hide && $peep_this_hide != 'false')
+		$content = $content;
+	else {
+		$align = pt_option('pt_alignment'); 
+		if ($align == '')	$align = 'left';
+		$p = '<p align="' . $align . '">'; $p2 = '</p>';
+		if (pt_option('pt_auto_display') == 'true')
+			$content .= pt_display_limits($p);
+		if (pt_option('pt_auto_display') == 'true')
+			$content .= peep_this() . '&nbsp;';
+		if (pt_option('pt_auto_display') == 'true')
+			$content .= pt_display_limits($p2);
+	}
+	return $content;
+}
+
+function peep_this_css() {
+	echo '<style type="text/css">a.peep-this{text-decoration:none;}</style>';
+}
+
+function update_pt_options() {
+	if(isset($_REQUEST['pt'])) $new_options = $_REQUEST['pt'];
+	$booleans = array('pt_30', 'pt_limit_to_single', 'pt_limit_to_posts', 'pt_auto_display');
+	foreach($booleans as $key)
+		$new_options[$key] = $new_options[$key] ? 'true' : 'false';
+	update_option('peep_this_settings', $new_options);
+	echo '<br /><div id="message" class="updated fade"><p>' .
+		__('Peep This options saved!', 'peep-this') . '</p></div>';}
+
+
+function pt_image_selection() {
+	$l = '/wp-content/plugins/peep-this/icons/pt-';
+	$checked = ' checked="checked"'; 
+	$u = get_option('siteurl');
+	$y = '.png" /></p><p><input type="radio" name="pt[pt_';
+	$z = '.png" /> <input type="radio" name="pt[pt_';
+	echo '<p><input type="radio" name="pt[pt_gatorpeeps_icon]" value="pt-gatorpeeps-big.png"';	
+	if (pt_option('pt_gatorpeeps_icon') == 'pt-gatorpeeps-big.png' || pt_option('pt_gatorpeeps_icon') == '') echo $checked;
+	echo ' /> <img src="' . $u . $l . 'gatorpeeps-big.png" alt="pt-gatorpeeps-big' . $z .
+	'gatorpeeps_icon]" value="pt-gatorpeeps-medium.png"';
+	if (pt_option('pt_gatorpeeps_icon') == 'pt-gatorpeeps-medium.png')
+	echo $checked;
+	echo ' /> <img src="' . $u . $l . 'gatorpeeps-medium.png" alt="pt-' .
+	'gatorpeeps-medium' . $z . 'gatorpeeps_icon]" value="pt-' . gatorpeeps . '-small.png"';
+	if (pt_option('pt_gatorpeeps_icon') == 'pt-gatorpeeps-small.png')
+	echo $checked; 
+	echo ' /> <img src="' . $u . $l . 'gatorpeeps-small.png" alt="pt-' .
+	'gatorpeeps-small' . $z . 'gatorpeeps_icon]" value="pt-gatorpeeps-text.png"';
+	if (pt_option('pt_gatorpeeps_icon') == 'pt-gatorpeeps-text.png')
+	echo $checked;
+	echo ' /> <img src="' . $u . $l . 'gatorpeeps-text.png" alt="pt-' .
+	'gatorpeeps-text.png" />';
+}
+
+function print_pt_form() {
+	if ($_REQUEST['reset'])
+		echo '<br /><div id="message" class="updated fade"><p>' .
+		__('Peep This options reset!', 'peep-this') . '</p></div>';
+	$s = ' selected="selected"';
+	$v = ' checked="checked"'; $w = pt_option('pt_url_www'); global $wpdb;
+	$count1 = number_format($wpdb->get_var("SELECT COUNT(*)
+		FROM $wpdb->posts WHERE post_status = 'publish'"));
+	$count2 = number_format($wpdb->get_var("SELECT COUNT(*) FROM
+		$wpdb->postmeta WHERE meta_key = 'peep_this_url' AND
+		meta_value != 'getnew'"));
+	if ($count2 > $count1) $count2 = $count1;
+	echo '<script type="text/javascript">var lastDiv = ""; function ' .
+	'showDiv(divName) {if (lastDiv) {document.getElementById(lastDiv).' .
+	'className = "hiddenDiv";} if (divName && document.getElementById' .
+	'(divName)) {document.getElementById(divName).className = "visible' .
+	'Div"; lastDiv = divName;}}</script><style type="text/css">label.t ' .
+	'{margin-top:5px;display:block;width:130px;padding:0;float:left;} ' .
+	'.hiddenDiv {display:none;} .visibleDiv {display:block;}</style><div' .
+	' class="wrap"><h2>' . __('Peep This Options', 'peep-this') .
+	'</h2><p>'; if ((ini_get('allow_url_fopen') == 0 ||
+	ini_get('allow_url_fopen') == 'off' || ini_get('allow_url_fopen') ==
+	'Off') && !function_exists('curl_init')) echo '<strong><font color="' .
+	'red">' . __('Allow_url_fopen and curl are disabled in your PHP ' .
+	'configuration.</font><br />All URLs will be served locally, ' .
+	'regardless of your chosen URL service.<br />To fix this, try ' .
+	'adding these lines to your <a href="http://www.washington.edu/' .
+	'computing/web/publishing/php-ini.html">php.ini file</a>:<br /> `' .
+	'extension = curl.so` and `allow_url_fopen = on`.' . '</strong>',
+	'peep-this') . '</font></p><p>';
+	echo '</p><form id="peep-this" name="peep-this" method="post" ' .
+	'action="">'; if (function_exists('wp_nonce_field'))
+		wp_nonce_field('update-options');
+	echo '<p><label class=' . '"t" for="pt[pt_alignment]">' . __('Alignment:', 'peep-this') .
+	'</label><select name="pt[pt_alignment]" id="pt[pt_alignment]">' .
+	'<option value="left"'; if (pt_option('pt_alignment') == 'left')
+	echo $s; echo '>' . __('Left', 'peep-this') . '</option><option ' .
+	'value="right"'; if (pt_option('pt_alignment') == 'right') echo $s;
+	echo '>' . __('Right', 'peep-this') . '</option><option value="' .
+	'center"'; if (pt_option('pt_alignment') == 'center') echo $s;
+	echo '>' . __('Center', 'peep-this') . '&nbsp;</option></select>' .
+	'</p><p><label><input type="checkbox" name="pt[pt_limit_to_single]"';
+	if (pt_option('pt_limit_to_single') == 'true') echo $v; echo ' /> ' .
+	__('Only show Peep This when viewing single posts or pages',
+	'peep-this') . '</label></p><p><label><input type="checkbox" name=' .
+	'"pt[pt_limit_to_posts]"';
+	if (pt_option('pt_limit_to_posts') == 'true') echo $v; echo ' /> ' .
+	__('Hide Peep This on pages', 'peep-this') .
+	'</label></p>';	pt_image_selection(); echo '<input type="hidden"' .
+	' name="action" value="update" /><input type="hidden" name="page_' .
+	'options" value="pt[pt_30],pt[pt_alignment],pt'.
+	'[pt_auto_display],pt[pt_limit_to_single],pt[pt_url_www],pt[pt_" />' .
+	'<p class="submit"><input type="submit" name="submit" value="' .
+	__('Save Options', 'peep-this') . '" /> <input type="submit" name="' .
+	'reset" value="' . __('Reset Options', 'peep-this') . '" onclick="' .
+	'return confirm (\'' . __('Are you sure you want to reset Peep This' .
+	' to its default settings?', 'peep-this') . '\');" /></p></form><p>'.
+	__('Visit the <a href="http://txdaily.co.za">Tx Daily</a> page for support.', 'peep-this').
+	'</p></div>';
+}
+
+function peep_this_install() {
+	$add_options = array('pt_alignment' => 'left',
+	'pt_twitter_icon' => 'pt-gatorpeeps-big.png', 'pt_limit_to_single' =>
+	'false','pt_limit_to_posts' => 'false');
+	foreach($add_options as $key => $value) {
+		if ($old = get_option($key)) {$add_options[$key] = $old;
+			delete_option($key);}}
+	if (get_option('peep_this_settings') == '')
+		add_option('peep_this_settings', $add_options);
+	delete_option('pt_add_title'); delete_option('pt_big_icon');
+	delete_option('pt_icon'); delete_option('pt_small_icon');
+}
+
+function peep_this_add_options() {
+	if (function_exists('add_options_page')) {
+		add_options_page(__('Peep This Options', 'peep-this'),
+			__('Peep This', 'peep-this'), 8,
+			__FILE__, 'peep_this_options');}
+}
+
+function peep_this_options() {
+	if ($_REQUEST['submit']) {
+		update_pt_options();}
+	if ($_REQUEST['reset']) {
+		delete_option('peep_this_settings'); global_flush_pt_cache();
+		peep_this_install();} print_pt_form();
+}
+
+// Sets one cached URL to "getnew". peep_this_short_url() respawns.
+function flush_pt_cache($post_id) {
+	$cached_pt_url = get_post_meta($post_id, 'peep_this_url', true);
+	if($cached_pt_url && $cached_pt_url != 'getnew') {
+	update_post_meta($post_id, 'peep_this_url', 'getnew');}
+}
+
+
+// Deletes the cached URL when you delete one post.
+function delete_pt_cache() {
+	global $id; delete_post_meta($id, 'peep_this_url');
+}
+
+
+// Sets every cached URL to "getnew". For permalink / URL service changes.
+function global_flush_pt_cache() {
+	global $wpdb; $wpdb->query("UPDATE $wpdb->postmeta
+	SET meta_value = 'getnew' WHERE meta_key = 'peep_this_url'");
+}
+
+
+// Deletes every cached URL. Triggered upon deactivation.
+function global_delete_pt_cache() {
+	global $wpdb;
+	// Careful here.
+	$wpdb->query("DELETE FROM $wpdb->postmeta " .
+	"WHERE meta_key = 'peep_this_url'");
+}
+
+add_action('admin_menu', 'peep_this_add_options');
+add_action('publish_post', 'flush_pt_cache');
+add_action('publish_future_post', 'flush_pt_cache');
+add_action('save_post', 'flush_pt_cache');
+add_action('edit_post', 'flush_pt_cache');
+add_action('delete_post', 'delete_pt_cache');
+add_action('generate_rewrite_rules', 'global_flush_pt_cache');
+add_action('wp_head', 'peep_this_css');
+add_filter('the_content', 'insert_peep_this');
+
+if (function_exists('register_activation_hook')) {
+	register_activation_hook(__FILE__, 'peep_this_install');}
+
+if (function_exists('register_deactivation_hook')) {
+	register_deactivation_hook(__FILE__, 'global_delete_pt_cache');}
+?>
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Peep This ===
+Contributors: Seags
+Donate link: none
+Tags: gatorpeeps, peeps, social, bookmarking
+Requires at least: 1.5
+Tested up to: 2.7.1
+Stable tag: 1.0
+
+Adds a "Peep This Post" link to every post and page, so your readers can share entries on Gatorpeeps. Shortens URLs in advance.
+
+== Description ==
+
+Adds a "Peep This" link to every post and page. Uses GatorURL to shorten the URL to save those precious characters. Customize under Settings > Peep This. Includes 4 buttons.
+
+NOTE: On WordPress 1.5, you must go to Options > Peep This and click "Save Options" before the Peep This links will appear.
+
+Copyright 2009  Seagyn Davis  ( email : sdavis@txsolutions.co.za )
+Freely released under Version 2 of the GNU General Public License as published
+by the Free Software Foundation, or, at your option, any later version.
+
+== Installation ==
+
+1. Upload the `peep-this` folder to `/wp-content/plugins/`.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. If you're using WordPress MU and want this plugin active for all blogs, move `peep-this.php` to `/wp-content/mu-plugins/` at this point.
+4. Peep This icons should automatically appear on every post and page! Go to Settings > Peep This to change stuff.
+
+== Version History ==
+
+1.0: 2009-05-04: First release.
+
+== Frequently Asked Questions ==
+
+= How does the cache work? =
+
+Cached short URLs are saved to the postmeta table when a visitor views posts. For any future pageloads, those URLs are loaded, instead of pinging the Th8.us server (or is.gd, TinyURL, etc.). This works, because as long as the post's permalink doesn't change, the short URL from the third-party service doesn't change.
+
+The cache is invalidated by setting the existing short URLs in the postmeta table to "getnew" as needed. By reusing the old fields instead of replacing them, I don't bump up the `meta_id` counter needlessly. When the next person visits that post, the `get_tweet_this_short_url` function in Tweet This sees this and gets a new short URL.
+
+What triggers a cached URL as invalid? When you save a post (including editing and publishing), the cache is invalidated in case you changed the permalink. Secondly, when you change URL services under Settings > Tweet This or change permalink structures under Options > Permalinks, all the cached URLs are set to "getnew". If you move your blog to a different directory or domain name, just change URL services and then change back to trigger a refresh on the cache.
+
+When you deactivate the plugin, all the cached URLs are deleted.
+
+= Why doesn't Tweet This show when I preview a draft post? =
+
+Because I'd have to fetch a short URL if it did, and the permalink for the post isn't set yet. It would be something like /?p=1, which would just waste an entry in GatorURL service's database.
+
+== Screenshots ==
+
+1. Peep This options page.
